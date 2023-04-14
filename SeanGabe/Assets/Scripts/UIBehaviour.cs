126e9b3d4af50c3ca430168a708314d9841c45f5
CreamLoPuff <clout9sb@gmail.com>
2023-04-13 23:04:50 -0500

    UI in proggress

---
+++ SeanGabe/Assets/Scripts/UIBehaviour.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SeanGabe");
+    }
+
+    public void loadControls()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+    
+    public void BacktoMain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
